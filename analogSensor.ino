76f26213808de653c72470c223fca26f7204216a
benjamin-moll <54908849+benjamin-moll@users.noreply.github.com>
2020-05-13 18:02:50 -0400

    Update analogSensor.ino

--- analogSensor.ino
+++ analogSensor.ino
@@ -5,6 +5,6 @@ Serial.begin(9600); //initialize serial communications
 void loop() {
   int potentiomter = analogRead(A0);                  //read the input pin
   int mappedPot = map(potentiometer,0,1023,0,255);    //remap the pot value to 0 to 255
-  Serial.println(mappedPot);                          //print it out to the serial monitor
+  Serial.write(mappedPot);                          //print it out to the serial monitor
   delay(1);                                           //slight delay to stabilize 
 }
